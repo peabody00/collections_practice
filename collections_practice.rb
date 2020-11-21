de5808541160017716ac6e5e437fac861525f9c8
Charlie <peabody00@gmail.com>
2020-11-20 21:55:01 -0600

    commit tree edf2af1b0e9bf6de1415bd51f1d3cf34b6187bbd

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+require 'pry'
 
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort do |a,b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort {|a,b| a.length <=> b.length}    
+end
+
+def swap_elements(array)
+    array[1],array[2]=array[2],array[1]
+    return array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each {|element| element[2] = "$"}     
+end
+
+def find_a(array)
+    array.select {|letter| letter.start_with?("a")}
+end
+
+def sum_array(array)
+    array.inject(:+)
+end
+
+def add_s(array)
+    new_array = []
+    array.each do |element|
+        if element == "feet"
+            new_array << element
+        else
+            new_array << element + "s"
+        end
+    end
+    #new_array[1] = "feet"
+    new_array
+end
